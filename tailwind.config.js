c8d584b6aeca3f75360152d9b175824d2cd64643
huitquatredev <julien@huitquatre.fr>
2022-06-23 09:39:18 +0200

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,7 @@ module.exports = {
   darkMode: 'class',
   content: ["./src/**/*.{html,njk}"],
   theme: {
-    extend: {
-      colors:{
-        'bg1-light': '#E5E7EB',
-        'bg1-dark': '#1F2937',
-        'neutral-light': '#000000',
-        'neutral-dark': '#ffffff',
-        'color': '#4ADE80' /* green-300 */
-      }
-    },
+    extend: {},
   },
-  plugins: [require('tailwindcss-neumorphism')],
+  plugins: [],
 }
