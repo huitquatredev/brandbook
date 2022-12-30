224cdb22483dbf9d698f6d6dd9a30ab8291daa44
huitquatredev <julien@huitquatre.fr>
2022-12-30 14:47:40 +0100

    commit

--- src/_includes/_main.js
+++ src/_includes/_main.js
@@ -1,6 +1,6 @@
 /* Download SVG */
 function downloadSVG(id) {
-  const svg = document.getElementById(id).outerHTML;
+  const svg = document.getElementById(id).innerHTML;
   const blob = new Blob([svg.toString()]);
   const element = document.createElement("a");
   element.download = `logo_${id}.svg`;
@@ -10,14 +10,14 @@ function downloadSVG(id) {
 }
 
 /* Fitty !!! */
-fitty('.fittext');
+fitty(".fittext");
 
 /* Handle the dark/light toggle button */
 const sun = document.querySelector(".feather-sun");
 const moon = document.querySelector(".feather-moon");
 sun.classList.toggle("hidden");
 
-function toggleDarkMode(){
+function toggleDarkMode() {
   document.body.classList.toggle("dark");
   sun.classList.toggle("hidden");
   moon.classList.toggle("hidden");
@@ -27,40 +27,39 @@ function toggleDarkMode(){
 /* Handle the theming pantone color of the year */
 /* theming preference is saved locally*/
 
-async function applyTheme(year){
+async function applyTheme(year) {
   //Get the root for changing CSS vars
-  const root = document.querySelector('html');
+  const root = document.querySelector("html");
 
   //Get the output element
-  const output = document.getElementById('theme');
+  const output = document.getElementById("theme");
 
   //Save the value in localStorage
   localStorage.theme = year;
   //Get the data from my json file
-  let resp = await fetch('/pantone-color-of-the-year.json');
+  let resp = await fetch("/pantone-color-of-the-year.json");
   let data = await resp.json();
   //Get the specific data for the input value
-  let coloroftheyear = data.filter(obj => {
-      return obj.year === year;
+  let coloroftheyear = data.filter((obj) => {
+    return obj.year === year;
   });
   //Show the year and the color name selected under the range slider
   output.value = `${year} ${coloroftheyear[0].name}`;
   //Change the css var colors
-  if(coloroftheyear.length === 1){
-    root.style.setProperty('--pantoned1',coloroftheyear[0].pantonedHex[5]);
-    root.style.setProperty('--pantoned2',coloroftheyear[0].pantonedHex[4]);
-    root.style.setProperty('--pantoned3',coloroftheyear[0].pantonedHex[3]);
-    root.style.setProperty('--pantoned4',coloroftheyear[0].pantonedHex[2]);
-    root.style.setProperty('--pantoned5',coloroftheyear[0].pantonedHex[1]);
-    root.style.setProperty('--pantoned6',coloroftheyear[0].pantonedHex[0]);
-  }
-  else{
-    root.style.setProperty('--pantoned1',coloroftheyear[1].pantonedHex[5]);
-    root.style.setProperty('--pantoned2',coloroftheyear[1].pantonedHex[4]);
-    root.style.setProperty('--pantoned3',coloroftheyear[1].pantonedHex[3]);
-    root.style.setProperty('--pantoned4',coloroftheyear[0].pantonedHex[2]);
-    root.style.setProperty('--pantoned5',coloroftheyear[0].pantonedHex[1]);
-    root.style.setProperty('--pantoned6',coloroftheyear[0].pantonedHex[0]);
+  if (coloroftheyear.length === 1) {
+    root.style.setProperty("--pantoned1", coloroftheyear[0].pantonedHex[5]);
+    root.style.setProperty("--pantoned2", coloroftheyear[0].pantonedHex[4]);
+    root.style.setProperty("--pantoned3", coloroftheyear[0].pantonedHex[3]);
+    root.style.setProperty("--pantoned4", coloroftheyear[0].pantonedHex[2]);
+    root.style.setProperty("--pantoned5", coloroftheyear[0].pantonedHex[1]);
+    root.style.setProperty("--pantoned6", coloroftheyear[0].pantonedHex[0]);
+  } else {
+    root.style.setProperty("--pantoned1", coloroftheyear[1].pantonedHex[5]);
+    root.style.setProperty("--pantoned2", coloroftheyear[1].pantonedHex[4]);
+    root.style.setProperty("--pantoned3", coloroftheyear[1].pantonedHex[3]);
+    root.style.setProperty("--pantoned4", coloroftheyear[0].pantonedHex[2]);
+    root.style.setProperty("--pantoned5", coloroftheyear[0].pantonedHex[1]);
+    root.style.setProperty("--pantoned6", coloroftheyear[0].pantonedHex[0]);
   }
 
   //Recalculate the contrast
@@ -70,52 +69,47 @@ async function applyTheme(year){
 //Thematisation des logos
 async function thematisation() {
   //Get the data from my json file
-  let resp = await fetch('/pantone-color-of-the-year.json');
+  let resp = await fetch("/pantone-color-of-the-year.json");
   let data = await resp.json();
   //Grab the logos
   const cont = document.getElementById("logoThemise");
   const logos = cont.querySelectorAll("svg");
   //Check if this is dark mode
-  const dark = document.body.classList.contains('dark');
+  const dark = document.body.classList.contains("dark");
   //Change the colors
-  for(let i=0;i<logos.length;i++){
-    logos[i].querySelectorAll(".logotype").forEach(letter => {
-      if(dark){
+  for (let i = 0; i < logos.length; i++) {
+    logos[i].querySelectorAll(".logotype").forEach((letter) => {
+      if (dark) {
         letter.style = `fill:${data[i].pantonedHex[5]};`;
-      }
-      else{
+      } else {
         letter.style = `fill:${data[i].pantonedHex[0]};`;
       }
     });
-    logos[i].querySelectorAll(".bottomleft").forEach(letter => {
-      if(dark){
+    logos[i].querySelectorAll(".bottomleft").forEach((letter) => {
+      if (dark) {
         letter.style = `fill:${data[i].pantonedHex[5]};`;
-      }
-      else{
+      } else {
         letter.style = `fill:${data[i].pantonedHex[0]};`;
       }
     });
-    logos[i].querySelectorAll(".bottomright").forEach(letter => {
-      if(dark){
+    logos[i].querySelectorAll(".bottomright").forEach((letter) => {
+      if (dark) {
         letter.style = `fill:${data[i].pantonedHex[4]};`;
-      }
-      else{
+      } else {
         letter.style = `fill:${data[i].pantonedHex[1]};`;
       }
     });
-    logos[i].querySelectorAll(".topleft").forEach(letter => {
-      if(dark){
+    logos[i].querySelectorAll(".topleft").forEach((letter) => {
+      if (dark) {
         letter.style = `fill:${data[i].pantonedHex[4]};`;
-      }
-      else{
+      } else {
         letter.style = `fill:${data[i].pantonedHex[1]};`;
       }
     });
-    logos[i].querySelectorAll(".topright").forEach(letter => {
-      if(dark){
+    logos[i].querySelectorAll(".topright").forEach((letter) => {
+      if (dark) {
         letter.style = `fill:${data[i].pantonedHex[3]};`;
-      }
-      else{
+      } else {
         letter.style = `fill:${data[i].pantonedHex[2]};`;
       }
     });
@@ -123,8 +117,7 @@ async function thematisation() {
 }
 thematisation();
 
-
-function handleThemingMobile(but){
+function handleThemingMobile(but) {
   //Get the input/output
   const input = but.getAttribute("year");
   //Apply Theming
@@ -133,21 +126,21 @@ function handleThemingMobile(but){
   toggleThemeMenu();
 }
 
-function handleThemingDesktop(){
+function handleThemingDesktop() {
   //Get the input/output
-  const input = document.getElementById('pantone-color-year').value;
+  const input = document.getElementById("pantone-color-year").value;
   //Apply Theming
   applyTheme(input);
 }
 
-function setSlider(year){
+function setSlider(year) {
   //Get the slider element and change the input
-  const handle = document.getElementById('pantone-color-year');
+  const handle = document.getElementById("pantone-color-year");
   handle.value = year;
 }
 
-function toggleThemeMenu(){
-  const themeMenu = document.getElementById('themeMenu');
+function toggleThemeMenu() {
+  const themeMenu = document.getElementById("themeMenu");
   themeMenu.classList.toggle("hidden");
 }
 
@@ -159,57 +152,54 @@ function RGBToHex(rgb) {
   rgb = rgb.substr(4).split(")")[0].split(sep);
 
   let r = (+rgb[0]).toString(16),
-      g = (+rgb[1]).toString(16),
-      b = (+rgb[2]).toString(16);
+    g = (+rgb[1]).toString(16),
+    b = (+rgb[2]).toString(16);
 
-  if (r.length == 1)
-    r = "0" + r;
-  if (g.length == 1)
-    g = "0" + g;
-  if (b.length == 1)
-    b = "0" + b;
+  if (r.length == 1) r = "0" + r;
+  if (g.length == 1) g = "0" + g;
+  if (b.length == 1) b = "0" + b;
 
   return "#" + r + g + b;
 }
 /* Convert Hex to rgb color */
 function hexToRgb(hex) {
   var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-  hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+  hex = hex.replace(shorthandRegex, function (m, r, g, b) {
     return r + r + g + g + b + b;
   });
 
   var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result ? {
-    r: parseInt(result[1], 16),
-    g: parseInt(result[2], 16),
-    b: parseInt(result[3], 16)
-  } : null;
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      }
+    : null;
 }
 
 /* Calculate luminance for a rgb color */
 function luminance(r, g, b) {
   var a = [r, g, b].map(function (v) {
-      v /= 255;
-      return v <= 0.03928
-          ? v / 12.92
-          : Math.pow( (v + 0.055) / 1.055, 2.4 );
+    v /= 255;
+    return v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4);
   });
   return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
 }
 
 /* Calculate the contrast ratio with luminance */
-function contrastLuminance(l1,l2) {
-  return(l1 < l2 ? (l2+0.05)/(l1+0.05) : (l1+0.05)/(l2+0.05));
+function contrastLuminance(l1, l2) {
+  return l1 < l2 ? (l2 + 0.05) / (l1 + 0.05) : (l1 + 0.05) / (l2 + 0.05);
 }
 
 /* Calculate the contrast ration between 2 hex colors */
-function contrastRatio(hex1,hex2) {
+function contrastRatio(hex1, hex2) {
   let rgb1 = hexToRgb(hex1);
   let rgb2 = hexToRgb(hex2);
-  let lum1 = luminance(rgb1.r,rgb1.g,rgb1.b);
-  let lum2 = luminance(rgb2.r,rgb2.g,rgb2.b);
-  
-  return(contrastLuminance(lum1,lum2));
+  let lum1 = luminance(rgb1.r, rgb1.g, rgb1.b);
+  let lum2 = luminance(rgb2.r, rgb2.g, rgb2.b);
+
+  return contrastLuminance(lum1, lum2);
 }
 
 function displayContrast() {
@@ -217,13 +207,22 @@ function displayContrast() {
   const contrast2 = document.getElementById("contrast2");
   const contrast3 = document.getElementById("contrast3");
 
-  let val1 = contrastRatio(getComputedStyle(document.documentElement).getPropertyValue('--pantoned6'),getComputedStyle(document.documentElement).getPropertyValue('--pantoned1'));
-  let val2 = contrastRatio(getComputedStyle(document.documentElement).getPropertyValue('--pantoned5'),getComputedStyle(document.documentElement).getPropertyValue('--pantoned2'));
-  let val3 = contrastRatio(getComputedStyle(document.documentElement).getPropertyValue('--pantoned3'),getComputedStyle(document.documentElement).getPropertyValue('--pantoned6'));
+  let val1 = contrastRatio(
+    getComputedStyle(document.documentElement).getPropertyValue("--pantoned6"),
+    getComputedStyle(document.documentElement).getPropertyValue("--pantoned1")
+  );
+  let val2 = contrastRatio(
+    getComputedStyle(document.documentElement).getPropertyValue("--pantoned5"),
+    getComputedStyle(document.documentElement).getPropertyValue("--pantoned2")
+  );
+  let val3 = contrastRatio(
+    getComputedStyle(document.documentElement).getPropertyValue("--pantoned3"),
+    getComputedStyle(document.documentElement).getPropertyValue("--pantoned6")
+  );
 
-  contrast1.value = Math.round(val1 * 10)/10;
-  contrast2.value = Math.round(val2 * 10)/10;
-  contrast3.value = Math.round(val3 * 10)/10;
+  contrast1.value = Math.round(val1 * 10) / 10;
+  contrast2.value = Math.round(val2 * 10) / 10;
+  contrast3.value = Math.round(val3 * 10) / 10;
 }
 displayContrast();
 
@@ -231,33 +230,35 @@ displayContrast();
 function hoverColorMatrix() {
   const colorMatrix = document.getElementById("colorMatrix");
   const children = [...colorMatrix.childNodes];
-  for(let i=1;i<children.length;i+=2){
-    if(i<12){
-      children[i].textContent = `pantoned${(i+1)/2}`;
-      children[i].addEventListener("mouseenter",(event) => {
-        event.target.textContent = RGBToHex(getComputedStyle(event.target).backgroundColor);
+  for (let i = 1; i < children.length; i += 2) {
+    if (i < 12) {
+      children[i].textContent = `pantoned${(i + 1) / 2}`;
+      children[i].addEventListener("mouseenter", (event) => {
+        event.target.textContent = RGBToHex(
+          getComputedStyle(event.target).backgroundColor
+        );
       });
-      children[i].addEventListener("mouseleave",(event) => {
-        event.target.textContent = `pantoned${(i+1)/2}`;
+      children[i].addEventListener("mouseleave", (event) => {
+        event.target.textContent = `pantoned${(i + 1) / 2}`;
+      });
+    } else {
+      children[i].textContent = `neutral${(i - 11) / 2}`;
+      children[i].addEventListener("mouseenter", (event) => {
+        event.target.textContent = RGBToHex(
+          getComputedStyle(event.target).backgroundColor
+        );
+      });
+      children[i].addEventListener("mouseleave", (event) => {
+        event.target.textContent = `neutral${(i + 1) / 2}`;
       });
     }
-    else{
-      children[i].textContent = `neutral${(i-11)/2}`;
-      children[i].addEventListener("mouseenter",(event) => {
-        event.target.textContent = RGBToHex(getComputedStyle(event.target).backgroundColor);
-      });
-      children[i].addEventListener("mouseleave",(event) => {
-        event.target.textContent = `neutral${(i+1)/2}`;
-      });
-    }    
   }
 }
 hoverColorMatrix();
 
-if (!('theme' in localStorage))  {
+if (!("theme" in localStorage)) {
   applyTheme(2022);
 } else {
   applyTheme(localStorage.theme);
   setSlider(localStorage.theme);
 }
-
